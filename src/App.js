6463ced3818b2d607745892a4b71743472534d2f
darellwest <ewere.njoagwu@gmail.com>
2023-11-16 17:23:22 +0100

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { Children, useEffect, useState } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 function App() {
+  const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState('');
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(users => setMonsters(m => users))
+  }, []);
+
+  function handleChange(e){
+    setSearchField(s => e.target.value);
+  }
+
+  const filteredMosters = monsters.filter(
+    monster => monster.name.toLowerCase().includes(searchField.toLowerCase())
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Monsters Rolodex</h1>
+      <SearchBox 
+        placeholder='search monsters'
+        handleChange={handleChange}
+      />
+      <CardList monsters={filteredMosters} />
     </div>
   );
 }
